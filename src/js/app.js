87c47a78f662de1d6cd8d3b8913463cbc696771d
Artem Kornishon <awaywee@gmail.com>
2024-06-15 21:30:26 +0300

    hw done

--- src/js/app.js
+++ src/js/app.js
@@ -4,64 +4,109 @@ const cardContent = document.querySelector('.column__card-content_add')
 const columns = document.querySelectorAll('.column')
 const columnTitle = document.querySelector('.column_title')
 const mainContainer = document.querySelector('.main_container')
+const toDoContainer = document.querySelector('.todo_container')
 let actualCard = null;
 const tasks = document.querySelectorAll('.column__card')
 
+  window.onload = loadFromLocalStorage();
+  
+
 addButton.addEventListener('click', function() {
-    const newColumnCard = document.createElement('div');
-    const randomId = 'card-' + Math.random().toString(36).substring(2, 9); 
-    columnTitle.insertAdjacentHTML("afterEnd", `<div class="column__card" draggable="true" id="${randomId}"><div class="column__card-content">Здесь будут прописаны задачи</div><button class="column__card-button"></button></div>`);
+  let columnCardText = document.getElementById('textareaid').value;
+  const newColumnCard = `<div class="column__card" draggable="true"><div class="column__card-content">${columnCardText}</div><button class="column__card-button"></button></div>`
+  columnTitle.insertAdjacentHTML("afterEnd", newColumnCard);
+  updateLocalStorage()
 });
 
 
 mainContainer.addEventListener('click', function(event) {
-    if (event.target.classList.contains('column__card-button')) {
-        const card = event.target.closest('.column__card');
-        if (card) {
-            card.remove();
-        }
-    } 
+  if (event.target.classList.contains('column__card-button')) {
+    const card = event.target.closest('.column__card');
+    if (card) {
+      card.remove();
+    }
+  }
+  updateLocalStorage()
 });
 
-function dragStart (e) {
-    e.dataTransfer.setData('text/plain', e.target.getAttribute('id'));
-    this.classList.add("is-dragging");
+function dragStart(e) {
+  actualCard = e.target
+  e.target.classList.add("is-dragging");
+  console.log(this, 'start')
 };
 
-function dragEnd () {
-    this.classList = 'column__card';
+function dragEnd(e) {
+  this.classList = 'column';
+  console.log(this, 'end')
 };
 
-function dragEnter (e) {
-    e.preventDefault();
-    this.classList.add('hovered');
+function dragEnter(e) {
+  e.preventDefault();
+  this.classList.add('hovered');
 };
 
-function dragLeave () {
-    this.classList.remove('hovered');
+function dragLeave() {
+  this.classList.remove('hovered');
 };
 
-function dragOver (e) {
-    e.preventDefault();
+function dragOver(e) {
+  e.preventDefault();
 };
-
-tasks.forEach(task => {
-    task.addEventListener('dragstart', dragStart);
-    task.addEventListener('dragend', dragEnd);
-});
 
 for (const column of columns) {
-    column.addEventListener('dragenter', dragEnter);
-    column.addEventListener('dragleave', dragLeave);
-    column.addEventListener('dragover', dragOver);
-    column.addEventListener('drop', drop);
+  column.addEventListener('dragenter', dragEnter);
+  column.addEventListener('dragleave', dragLeave);
+  column.addEventListener('dragover', dragOver);
+  column.addEventListener('drop', drop);
+  column.addEventListener('dragstart', dragStart);
+  column.addEventListener('dragend', dragEnd);
 }
 
-function drop (e) {
+
+function drop(e) {
     e.preventDefault();
+    e.target.appendChild(actualCard);
     this.classList = 'column'
-    const id = e.dataTransfer.getData('text/plain');
-    console.log(id)
-    e.target.append(document.getElementById(id))
+    actualCard.classList.remove("is-dragging");
+    actualCard = null;
+    updateLocalStorage()
 };
 
+function updateLocalStorage() {
+    const condition = [];
+    const columnLine = document.querySelectorAll(".column"); // столбцы с карточками
+    console.log(columnLine)
+    columnLine.forEach((line, indexLine) => {
+      condition[indexLine] = [];
+      console.log(condition)
+      let taskCard = line.querySelectorAll(".column__card"); // карточки в каждом столбце
+      taskCard.forEach((card, indexCard) => {
+        let content = card.querySelector(".column__card-content").textContent
+        console.log(card)
+        console.log(content)
+        let dataBody = [content];
+        condition[indexLine][indexCard] = dataBody;
+      });
+    });
+    let database = JSON.stringify(condition);
+    window.localStorage.setItem("database", database);
+  };
+
+  function loadFromLocalStorage() {
+    if (!localStorage.getItem("database")) return;
+    let getDataBase = localStorage.getItem("database");
+    let dataBase = JSON.parse(getDataBase);
+    for (let i = 0; i < dataBase.length; i++) {
+      for (let j = 0; j < dataBase[i].length; j++) {
+        columns[i].querySelector(".column_title").insertAdjacentHTML(
+          "afterEnd",
+          `<div class="column__card" draggable="true"><div class="column__card-content">${dataBase[i][j]}</div><button class="column__card-button"></button></div>`
+        );
+      }
+    }
+  };
+  
+  
+  
+  
+  
